9f3d547df6729f36f8b275d9be0c9a73a932ce21
GEND1T <mafinaditya1606@gmail.com>
2026-01-18 18:57:40 +0700

    uas

---
+++ frontend-uas/astro.config.mjs
@@ -0,0 +1,14 @@
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
+  integrations: [
+    tailwind() // Gunakan integrasi resmi di sini
+  ],
+});
